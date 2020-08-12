2a00d994fa181071fade825d38ff3bda6c7a1f48
uriel077 <uriel07777@gmail.com>
2020-08-12 17:33:24 +0300

    story agent

---
+++ Assets/Scenes/Scripts/storyClass/StoryAgent.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StoryAgent : MonoBehaviour
+{
+   public GameObject storyManager;
+    private static bool created = false;
+    
+    // Start is called before the first frame update
+    void awake()
+    {
+        manageSingleController();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    /// <summary>
+    /// only one story controller 
+    /// </summary>
+    void manageSingleController()
+    {
+        // Ensure the script is not deleted while loading.
+        if (!created)
+        {
+            created = true;
+        }
+        else
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
